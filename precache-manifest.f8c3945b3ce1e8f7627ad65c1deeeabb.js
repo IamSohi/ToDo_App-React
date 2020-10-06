b59edc3f731dcee546111a7b45fa727e59643fdb
IamSohi <sukhveersinghsohi70@gmail.com>
2020-10-06 05:00:27 -0700

    Updates

---
+++ precache-manifest.f8c3945b3ce1e8f7627ad65c1deeeabb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bb7f496c96f2e644dd0556d049dc1d",
+    "url": "/ToDo_App-React/index.html"
+  },
+  {
+    "revision": "a148fde55682c8e9140d",
+    "url": "/ToDo_App-React/static/css/main.d0dbe632.chunk.css"
+  },
+  {
+    "revision": "3bbd49a5838619db0610",
+    "url": "/ToDo_App-React/static/js/2.13a34f16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo_App-React/static/js/2.13a34f16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a148fde55682c8e9140d",
+    "url": "/ToDo_App-React/static/js/main.d0019a78.chunk.js"
+  },
+  {
+    "revision": "988cebdbb5a4ba3f7876",
+    "url": "/ToDo_App-React/static/js/runtime-main.b248b94b.js"
+  }
+]);
